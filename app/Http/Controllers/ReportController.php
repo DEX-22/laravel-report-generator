45dd38e196da1939d89880fc61c1f441fb6d7bac
EnriqueSaenz <enriquesaenz0@gmail.com>
2026-01-21 13:06:55 -0500

    feat: generate report in background

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exports\GeneralReportExport;
 use App\Http\Requests\ReportByRangeRequest;
+use App\Jobs\GenerateAndDownloadReport;
 use App\Services\IReportService;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
@@ -14,7 +15,17 @@ class ReportController extends Controller
         return view('welcome');
     }
     public function getReportByRange(ReportByRangeRequest $request){
+        GenerateAndDownloadReport::dispatch($request->start,$request->end);
 
-        return Excel::download(new GeneralReportExport($request->start,$request->end), 'General report.xlsx');
+        return response();
+    }
+    public function downloadSavedReport($folder,$fileName){
+        $url = storage_path("$folder/$fileName");
+        
+        if (!file_exists($url)) {
+            abort(404);
+        }
+
+        return response()->download($url)->deleteFileAfterSend(true);    
     }
 }
---
+++ app/Jobs/GenerateAndDownloadReport.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\FailedDownloadEvent;
+use App\Events\FinishDownloadEvent;
+use App\Exports\GeneralReportExport;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\URL;
+use Maatwebsite\Excel\Facades\Excel;
+
+class GenerateAndDownloadReport implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public string $start,
+        public string $end
+    )
+    {
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $folder = "reports";
+        $fileName = "General report ({$this->start}-{$this->end}).xlsx";
+        $url = storage_path("$folder/$fileName");
+        $result = Excel::store(new GeneralReportExport($this->start,$this->end), $url);
+
+        if($result){
+            $signedUrl = URL::temporarySignedRoute('general.report.download',now()->addMinutes(10),[
+                'folder' => $folder, 'filename' => $fileName
+            ]); 
+
+            broadcast(new FinishDownloadEvent($signedUrl));
+        }else{
+            broadcast(new FailedDownloadEvent());
+        }
+    } 
+}
